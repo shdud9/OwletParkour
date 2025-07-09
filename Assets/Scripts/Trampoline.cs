463826441f1b795e843cc8e610177549d3237d56
1L1S <ilusha.chap@gmail.com>
2025-07-09 17:47:23 +0200

    UnusedMetodsDeleted

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 public class Trampoline : MonoBehaviour
 {
     public float jumpForce = 10f;
-    //видали не потрібний метод
-    void Start()
-    {
-        
-    }
-    
-    //видали не потрібний метод
-
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))

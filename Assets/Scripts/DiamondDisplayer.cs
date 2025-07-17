7aee217df990e2e058dfa81f72865fcdaea4fbea
1L1S <ilusha.chap@gmail.com>
2025-07-17 23:31:19 +0200

    DiamondsNotWork

---
+++ Assets/Scripts/DiamondDisplayer.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+
+public class DiamondDisplayer : MonoBehaviour
+{
+    string diamondKey = "diamondKey";
+    public TextMeshProUGUI diamondText;
+    private int diamonds = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        diamonds = PlayerPrefs.GetInt(diamondKey, 0);
+        diamondText.text = diamonds.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CollectCoins()
+    {
+        diamonds++;
+        diamondText.text = diamonds.ToString();
+        
+        
+    }
+}
